3298ff1e786e0f775dc51db471bd922bd35238ee
vovinhd <timo.volk@gmail.com>
2022-05-26 08:32:48 +0200

    spinner

--- src/routes/index@root.svelte
+++ src/routes/index@root.svelte
@@ -8,4 +8,6 @@
 	});
 </script>
 
-<h1>Index</h1>
+<div class="grid content-center w-full h-full absolute inset-0 ">
+	<div class="mx-auto w-24 h-24 border-4 border-water2 border-r-2  rounded-full animate-spin" />
+</div>
