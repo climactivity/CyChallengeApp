fcb06fe2e8debdf55077a49223367f8e40429b4d
vovinhd <timo.volk@gmail.com>
2022-07-29 12:01:05 +0200

    make native back forward disablebe by env

--- src/lib/plugins/EnablePlugins.ts
+++ src/lib/plugins/EnablePlugins.ts
@@ -1,13 +1,19 @@
-import { Capacitor } from "@capacitor/core";
-import NativeBackGesturePlugin from "./NativeBackGesture";
+import { Capacitor } from '@capacitor/core';
+import NativeBackGesturePlugin from './NativeBackGesture';
 const enablePlugins = async () => {
-    if (Capacitor.getPlatform() === "ios") {
-        console.log("Registering iOS plugins");
-        NativeBackGesturePlugin
-            .enable()
-            .then(() => {console.log("NativeBackGesturePlugin enabled");})
-            .catch((e) => {console.error("NativeBackGesturePlugin error: " + e);});
-    }
-}
+	if (
+		Capacitor.getPlatform() === 'ios' &&
+		import.meta.env.VITE_IOS_NATIVE_BACK_FORWARD === 'true'
+	) {
+		console.log('Registering iOS plugins');
+		NativeBackGesturePlugin.enable()
+			.then(() => {
+				console.log('NativeBackGesturePlugin enabled');
+			})
+			.catch((e) => {
+				console.error('NativeBackGesturePlugin error: ' + e);
+			});
+	}
+};
 
-export default enablePlugins; 
+export default enablePlugins;
