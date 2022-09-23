427b9449f7d0fedc43d917cdbece0e8f45bf9521
vovinhd <timo.volk@gmail.com>
2022-09-23 11:31:31 +0200

    new challenges are finally in

--- src/lib/components/challenge/ChallengeV2Todos.svelte
+++ src/lib/components/challenge/ChallengeV2Todos.svelte
@@ -43,7 +43,7 @@
 <div>
 	<div class="text-center mx-auto font-serif font-semibold text-2xl">Todos</div>
 	<div class="grid grid-flow-row text-md items-center  gap-2">
-		<!-- <Confetti bind:playAt /> -->
+		<!-- <Confetti b	ind:playAt /> -->
 
 		{#if _difficulty && _difficulty.todos}
 			{#each _difficulty.todos as step}
