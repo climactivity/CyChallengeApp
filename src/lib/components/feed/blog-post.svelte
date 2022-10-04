1b5a1f300518d1c677413988349f1f95770342bf
vovinhd <timo.volk@gmail.com>
2022-10-04 12:02:29 +0200

    added inapp browser plugin

--- src/lib/components/feed/blog-post.svelte
+++ src/lib/components/feed/blog-post.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import type { Post } from '$lib/types/post';
 	import { DateTime } from 'luxon';
+	import { Browser } from '@capacitor/browser';
 
 	export let post: Post;
 
-	// console.log(post);
+	const openInappBrowser = async () => {
+		await Browser.open({ url: post.link });
+	};
 
 	const getDateString = (date) => {
 		return DateTime.fromISO(date).setLocale('de').toLocaleString(DateTime.DATE_FULL);
@@ -20,37 +23,33 @@
 </script>
 
 <!-- svelte-ignore empty-block -->
-{#if post}
-	<a class="blog-post mt-2 bg-gray-50 rounded-sm shadow-md " href={post.link}>
-		{#if post.featured_media !== 0}
-			<img src={post._embedded['wp:featuredmedia'][0].source_url} alt="Post img" class="w-24" />
-		{:else}
-			<img src="/images/not-found.webp" alt="Post img" class="w-24" />
-		{/if}
-		<div class="p-2 h-32">
-			<h1 class="title break-words ">
-				{@html post.title.rendered.slice(0, 50)}
-				{#if post.title.rendered.length >= 50}
-					<span class="">...</span>
-				{/if}
-			</h1>
+<div class="blog-post mt-2 bg-gray-50 rounded-sm shadow-md " on:click={openInappBrowser}>
+	{#if post.featured_media !== 0}
+		<img src={post._embedded['wp:featuredmedia'][0].source_url} alt="Post img" class="w-24" />
+	{:else}
+		<img src="/images/not-found.webp" alt="Post img" class="w-24" />
+	{/if}
+	<div class="p-2 h-32">
+		<h1 class="title break-words ">
+			{@html post.title.rendered.slice(0, 50)}
+			{#if post.title.rendered.length >= 50}
+				<span class="">...</span>
+			{/if}
+		</h1>
 
-			<div class="flex flex-col text-sm text-gray-500">
-				<span>{estimateReadingTime(post.content.rendered)}</span>
-				<span>{getDateString(post.date)}</span>
-			</div>
-			<!-- 
+		<div class="flex flex-col text-sm text-gray-500">
+			<span>{estimateReadingTime(post.content.rendered)}</span>
+			<span>{getDateString(post.date)}</span>
+		</div>
+		<!-- 
 			<p>
 				{@html post.excerpt.rendered.slice(0, 75)}
 				{#if post.excerpt.rendered.length >= 75}
 					<span class="">...</span>
 				{/if}
 			</p> -->
-		</div>
-	</a>
-{:else}
-	<div class="flex items-center mt-2 w-44 animate-pulse bg-gray-50 rounded-sm " />
-{/if}
+	</div>
+</div>
 
 <style>
 	.blog-post {
