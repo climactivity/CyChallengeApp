1661937d713c67f53f217bd11ab40381c6c222cf
vovinhd <timo.volk@gmail.com>
2022-10-05 15:02:07 +0200

    new dates

--- src/lib/components/challenge/date-picker-section.svelte
+++ src/lib/components/challenge/date-picker-section.svelte
@@ -24,12 +24,16 @@
 		const dateTime = DateTime.now().setZone('Europe/Berlin');
 		const { type, notificationDays } = challenge;
 
-		if (type === 'one-time') {
+		console.log('calcDateOptionsForChallenge', challenge);
+		if (type === 'one-time' || type === 'repeatable') {
 			return notificationDays.map((days) => {
-				{
-					value: dateTime.plus({ days }).endOf('day').toISO();
-					display: `in ${days}`;
-				}
+				const result = dateTime.plus({ days }).startOf('day').plus({ hours: 16 });
+				//.toUTC()
+
+				return {
+					value: result.toFormat("yyyy-MM-dd'T'HH:mm"),
+					display: `${result.toRelativeCalendar()}`
+				};
 			});
 		}
 
@@ -39,22 +43,32 @@
 
 <VSection {...$$props}>
 	<div class="text-2xl text-bold font-semibold font-serif">Wir gucken wie es dir geht am:</div>
-	<ul>
-		{#each calcDateOptionsForChallenge(challenge) as day}
-			<li>{day}</li>
-		{/each}
-	</ul>
+	<ul />
 	<div>
 		<form>
+			<select
+				name="dates"
+				id="date-select"
+				bind:value={selectedDate}
+				on:change={() => {
+					onchanged(selectedDate);
+					setNewNextCheckpoint(challenge, selectedDate);
+				}}
+			>
+				{#each calcDateOptionsForChallenge(challenge) as day}
+					<option value={day.value}>{day.display}</option>
+				{/each}
+			</select>
+
 			<!-- todo replace -->
-			<input
+			<!-- <input
 				type="datetime-local"
 				bind:value={selectedDate}
 				on:change={() => {
 					onchanged(selectedDate);
 					setNewNextCheckpoint(challenge, selectedDate);
 				}}
-			/>
+			/> -->
 		</form>
 	</div>
 
