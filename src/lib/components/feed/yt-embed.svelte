2aeb98e707db808c1ea45fdc9a621fdce8c0f989
vovinhd <timo.volk@gmail.com>
2022-10-20 14:58:20 +0200

    added embetty

--- src/lib/components/feed/yt-embed.svelte
+++ src/lib/components/feed/yt-embed.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
+	import EmbettyYoutube from '../embetty-svelte/embetty-youtube.svelte';
+
 	let latestVideo = 'EaGnkNQPdsQ';
 	let frameSource = `https://www.youtube.com/embed/${latestVideo}`;
 </script>
 
 <!-- <span>{latestVideo}</span> -->
 <div class="m-4">
-	<iframe
+	<EmbettyYoutube videoId={latestVideo} />
+
+	<!-- <iframe
 		class="w-full aspect-video shadow-lg rounded-lg"
 		src={frameSource}
 		title="climactivity - Gemeinsam zum Klimaziel"
 		frameborder="0"
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 		allowfullscreen
-	/>
+	/> -->
 </div>
