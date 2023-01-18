93c74d1669df0a0f60cfeda7d11bc0e41704de31
vovinhd <timo.volk@gmail.com>
2023-01-18 12:50:15 +0100

    network error handling

--- src/lib/components/dialogs/network-error.svelte
+++ src/lib/components/dialogs/network-error.svelte
@@ -9,11 +9,10 @@
 
 	let isOpen = false;
 	let state: { error: string; description: string } = null;
-
-	hasError.subscribe((error) => {
-		if (error != null) {
+	hasError.subscribe((errorValue) => {
+		if (errorValue != null) {
 			isOpen = true;
-			state = error;
+			state = errorValue;
 		} else {
 			isOpen = false;
 			state = null;
@@ -26,22 +25,24 @@
 		style={'position: absolute; inset: 0; background-color: rgb(0 0 0); opacity: 0.3; z-index: 51'}
 	/>
 
-	<div
-		class="absolute left-8 right-8 bottom-20 h-60 bg-white rounded-lg p-4 space-y-4 shadow-xl z-[52]"
-	>
-		<DialogTitle class="text-xl font-bold">
-			<div class="flex flex-row place-content-between">
-				<div>{state.error}</div>
-			</div>
-		</DialogTitle>
-		<DialogDescription>{state.description}</DialogDescription>
-		<div class="text-center ">
-			<div class="lds-ripple">
-				<div />
-				<div />
+	{#if state && state.error && state.description}
+		<div
+			class="absolute left-8 right-8 bottom-20 h-60 bg-white rounded-lg p-4 space-y-4 shadow-xl z-[52]"
+		>
+			<DialogTitle class="text-xl font-bold">
+				<div class="flex flex-row place-content-between">
+					<div>{state?.error}</div>
+				</div>
+			</DialogTitle>
+			<DialogDescription>{state.description}</DialogDescription>
+			<div class="text-center ">
+				<div class="lds-ripple">
+					<div />
+					<div />
+				</div>
 			</div>
 		</div>
-	</div>
+	{/if}
 </Dialog>
 
 <style lang="scss">
