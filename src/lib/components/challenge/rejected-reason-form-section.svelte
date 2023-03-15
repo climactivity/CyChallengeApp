e880254afd7b57e19a3e1c6468e9d443fc7f05bd
vovinhd <timo.volk@gmail.com>
2023-03-15 10:33:58 +0100

    fixed reject form

--- src/lib/components/challenge/rejected-reason-form-section.svelte
+++ src/lib/components/challenge/rejected-reason-form-section.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
 	import VSection from '$lib/components/challenge/v-section.svelte';
+	import { writeStorage } from '$lib/services/client-storage-engine';
 	import type { ChallengeV2 } from '$lib/types/challenges';
+	import { fade } from 'svelte/transition';
+	import { generateUUID } from 'three/src/math/MathUtils';
 	export let challenge: ChallengeV2;
 
 	let reason;
 	let message = '';
+
+	let rejectionRecorded = false; 
+	let submitting = false
+	const saveRejectReason = async (challenge, reason, message) => { 
+		submitting = true;
+		const key = `${challenge.slug}-${generateUUID()}`
+		await writeStorage("challenge-rejects", key, {
+			challenge,
+			reason,
+			message,
+		}, null, 1,0 ).catch(e => console.error(e))
+		rejectionRecorded = true; 
+		submitting = false; 
+	} 
 </script>
 
 <VSection {...$$props}>
 	<div class="grid grid-flow-col ">
+		{#if rejectionRecorded}
+		<div transition:fade>Danke, wir haben dein Feedback erhalten!</div>
+		{:else}
 		<form class="w-full gap-4 flex flex-col">
 			<label for="reason"
 				>Warum nicht?
@@ -37,18 +57,23 @@
 					<button
 						class=" rounded shadow mx-8  py-2 bg-white text-lg animate-fadeInDown "
 						type="submit"
+						disabled={submitting}
 						on:click|preventDefault={(e) => {
 							console.log({
 								ch: challenge.slug,
 								reason,
 								message
 							});
+
+							saveRejectReason(challenge, reason, message);
 						}}
 					>
-						Absenden
+						{submitting? "Sende Feedback..." : "Absenden"}
 					</button>
 				</div>
 			{/if}
 		</form>
+		{/if}
+
 	</div>
 </VSection>
